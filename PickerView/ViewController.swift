c0b6c58364d1132db80605d7aea49dcecbd7ced8
D7703_02 <d7703_02@D7703-02ui-iMac.local>
2018-04-18 15:43:05 +0900

    if

--- PickerView/ViewController.swift
+++ PickerView/ViewController.swift
@@ -1,25 +1,55 @@
-//
-//  ViewController.swift
-//  PickerView
-//
-//  Created by D7703_02 on 2018. 4. 18..
-//  Copyright © 2018년 D7703_02. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var yourLabel: UILabel!
+    
+    var color = ["white", "GREEN","RED"]
+    var feeling = ["1","2","3","4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+    }
+    
+
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 3
+        }else {
+            return 4
+        }
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+    
+        if component == 0 {
+            return color[row]
+        }else {
+            return feeling[row]
+        }
+        
     }
-
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        if component == 0 {
+            myLabel.text = color[row]
+            
+        }else {
+        
+        yourLabel.text = feeling[row]
+        }
 }
 
+}
